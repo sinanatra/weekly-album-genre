54beb7e9555f8c929f1dc29246427541cd3eb5b5
Giacomo Nanni <gn.nanni@gmail.com>
2024-03-24 15:18:56 +0100

    fix added ids from json

--- notebooks/albumGenre.py
+++ notebooks/albumGenre.py
@@ -23,8 +23,20 @@ for link in soup.findAll('a'):
     except:
         continue
 
+# take data from the json
+script_tag = soup.find('script', id='__NEXT_DATA__')
+script_content = script_tag.string
+data = json.loads(script_content)
+page_props_data = data['props']['pageProps']['data']['artists']['short']
+
+for link in page_props_data:
+    links.append(link['id'])
+
+#print(len(links))
+
 cid = os.environ['SPOTIFY_API_KEY']
 secret = os.environ['SPOTIFY_API_SECRET']
+
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
@@ -33,9 +45,7 @@ dictionary = {}
 for id in list(set(links)):
     try:
         artist = sp.artist(id)
-
-        print("genres:", artist['genres'])
-
+        #print("genres:", artist['genres'])
         for i in artist['genres']:
             if i not in dictionary:
                 dictionary[i] = [ id]
